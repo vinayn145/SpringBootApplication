b487dfe463347b91e00dc3b33c86a6cf906b71bf
vinayn145 <vinayn145@gmail.com>
2023-03-14 21:16:03 +0530

    Adding annotations and of SpringBootApplication

--- src/main/java/com/vinay/Main.java
+++ src/main/java/com/vinay/Main.java
@@ -1,17 +1,24 @@
 package com.vinay;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+//@SpringBootApplication is a comprise of together 3 annotations namely @EnableAutoConfiguration,@ComponentScan,@Configuration
+@ComponentScan(basePackages = "com.vinay")
+@EnableAutoConfiguration
+@Configuration
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
-@GetMapping("/")
+@GetMapping("/greet")
     public String greet(){
      return "Hello";
  }
