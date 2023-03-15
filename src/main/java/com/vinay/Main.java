6d486565fc0ab44b8e0205845a6bf5ac0a4a3225
vinayn145 <vinayn145@gmail.com>
2023-03-15 20:06:46 +0530

    Json for Java- showing of record greetResponse by demonstrating GreetResponse class

--- src/main/java/com/vinay/Main.java
+++ src/main/java/com/vinay/Main.java
@@ -1,12 +1,13 @@
 package com.vinay;
 
+import io.micrometer.observation.transport.Propagator;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cglib.core.HashCodeCustomizer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 //@SpringBootApplication
 //@SpringBootApplication is a comprise of together 3 annotations namely @EnableAutoConfiguration,@ComponentScan,@Configuration
@@ -20,5 +21,36 @@ public class Main {
     public GreetResponse greet(){
      return new GreetResponse("Hello");
  }
- record GreetResponse(String greet){}
+ // record GreetResponse(String greet){}
+    class GreetResponse{
+        private final String greet;
+
+     GreetResponse(String greet) {
+         this.greet = greet;
+     }
+
+     public String getGreet() {
+         return greet;
+     }
+
+     @Override
+     public String toString() {
+         return "GreetResponse{" +
+                 "greet='" + greet + '\'' +
+                 '}';
+     }
+
+     @Override
+     public boolean equals(Object o) {
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         GreetResponse that = (GreetResponse) o;
+         return Objects.equals(greet, that.greet);
+     }
+
+     @Override
+     public int hashCode() {
+         return Objects.hash(greet);
+     }
+ }
 }
