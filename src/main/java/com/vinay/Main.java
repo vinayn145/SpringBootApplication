32fe882148110791acbb31039a5d98985f4cc406
vinayn145 <vinayn145@gmail.com>
2023-03-17 11:25:18 +0530

    Creating Json Objects

--- src/main/java/com/vinay/Main.java
+++ src/main/java/com/vinay/Main.java
@@ -7,6 +7,7 @@ import org.springframework.cglib.core.HashCodeCustomizer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Objects;
 
 //@SpringBootApplication
@@ -18,39 +19,20 @@ public class Main {
         SpringApplication.run(Main.class,args);
     }
 @GetMapping("/greet")
-    public GreetResponse greet(){
-     return new GreetResponse("Hello");
+    public GreetResponse greet() {
+           GreetResponse response =new GreetResponse(
+                "Hello",
+                List.of("Java","Python","Javascript"),
+                new Person("vinay",24,30_000)
+    );
+    return response;
  }
- // record GreetResponse(String greet){}
-    class GreetResponse{
-        private final String greet;
+  record Person(String name,int age,double savings){
 
-     GreetResponse(String greet) {
-         this.greet = greet;
-     }
-
-     public String getGreet() {
-         return greet;
-     }
-
-     @Override
-     public String toString() {
-         return "GreetResponse{" +
-                 "greet='" + greet + '\'' +
-                 '}';
-     }
-
-     @Override
-     public boolean equals(Object o) {
-         if (this == o) return true;
-         if (o == null || getClass() != o.getClass()) return false;
-         GreetResponse that = (GreetResponse) o;
-         return Objects.equals(greet, that.greet);
-     }
-
-     @Override
-     public int hashCode() {
-         return Objects.hash(greet);
-     }
- }
+  }
+ record GreetResponse(
+          String greet,
+          List<String> favProgrammingLanguages,
+          Person person
+          ){}
 }
